0ef2e5b3a71986a633e5037f2deb94fed6cb0ede
uniuni76 <e155766@ie.u-ryukyu.ac.jp>
2019-11-24 15:49:33 +0900

    change package cli

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-   "fmt"
-   "os"
-
-   "github.com/nasum/culc/cmd"
+    "os"
+    "github.com/urfave/cli"
 )
 
 func main() {
-  if err := cmd.RootCmd.Execute(); err != nil {
-      fmt.Fprint(os.Stderr, "%s: %v\n", os.Args[0], err)
-      os.Exit(-1)
-  }
+  app := cli.NewApp()
+  app.Run(os.Args)
 }
