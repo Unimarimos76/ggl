9748516593005d883af991258cf95cd92ea0aff2
uniuni76 <e155766@ie.u-ryukyu.ac.jp>
2020-01-13 16:03:18 +0900

    commit

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"github.com/urfave/cli"
+//	"github.com/urfave/cli"
 	"os"
 	"os/exec"
 )
 
-func main() {
-	app := cli.NewApp()
-	app.Name = "sampleApp"
-	app.Usage = "This app echo input arguments"
-	app.Version = "0.0.1"
+//func main() {
+//	app := cli.NewApp()
+//	app.Name = "sampleApp"
+//	app.Usage = "This app echo input arguments"
+//	app.Version = "0.0.1"
+//
+//	app.Action = func(context *cli.Context) error {
+//		err := exec.Command("open", "https://www.google.com/search?q="+context.Args().Get(0)).Start()
+//		if err != nil {
+//			panic(err)
+//		}
+//		return nil
+//	}
+//
+//	app.Run(os.Args)
+//}
 
-	app.Action = func(context *cli.Context) error {
-		err := exec.Command("open", "https://www.google.com/search?q="+context.Args().Get(0)).Start()
-		if err != nil {
-			panic(err)
-		}
-		return nil
-	}
 
-	app.Run(os.Args)
+func mian() {
+
 }
