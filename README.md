732338abb61f7becb91e93bde2e4f385d3c1be9f
uniuni76 <e155766@ie.u-ryukyu.ac.jp>
2020-02-23 03:52:05 +0900

    fix README.md

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ggl-command-line tool
 You can jump to Chrome search by typing ggl hoge on the command line.
+
+日本では調べたらわかる質問などに対してggrksという言葉で返すことがあります。ggrksという言葉の意味はGoogle Chromeで調べてみてくれなどの意味を持ちます。
+この言葉を用いてggrという名前にしました。
+
+
+# Demo
+
+
 # Usage
 ```
 ggl hoge fuha hoge 
 ```
-
 
--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
-import ( 
-    "flag"
-    "os/exec"
-    "strings"
+import (
+	"flag"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-   flag.Parse()
-   var str string
-   args := flag.Args()
-   str = strings.Join(args, " ")
-  err := exec.Command("open", "https://www.google.com/search?q="+ str).Start()
-   if err != nil {
-           panic(err)
-   }
+	flag.Parse()
+	var str string
+	var pri = flag.Bool("p", true, "private")
 
+	args := flag.Args()
+	str = strings.Join(args, " ")
+	err := exec.Command("open", "https://www.google.com/search?q="+str).Start()
+	if err != nil {
+		panic(err)
+	}
 }
-
